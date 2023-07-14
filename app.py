8407f2481d1f41e0735d017e11d5837d31b4b0b3
maelam-t431 <101439274@georgebrown.ca>
2023-07-14 13:54:59 -0400

    load model weights

--- app.py
+++ app.py
@@ -14,3 +14,14 @@ model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
 model.add(Dense(10, activation='softmax'))
 
+# Load the model weights from file
+model.load_weights('model.h5')
+
+
+st.set_page_config(layout="wide", page_title="MNIST Classifier")
+
+st.write("## Classify your images")
+st.write(
+    "Try uploading an image of a handwritten digit, the model will predict which digit it is."
+)
+st.sidebar.write("## Upload your image")
